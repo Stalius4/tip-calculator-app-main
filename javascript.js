902a85fc498773cd9958ecc755d4dad8f5185af0
Stalius4 <deivydasrimgaila@gmail.com>
2022-04-07 15:48:57 +0100

    Working custom button with calculations. Missing on change JS  event for Number of people and bill

--- javascript.js
+++ javascript.js
@@ -6,7 +6,8 @@ const zero = document.querySelector("#zero-top")
 const zeroNumOfPeople = document.querySelector("#zero-mid")
 const inputs = document.querySelectorAll(".input")
 let customBtn = document.querySelector("#custom")
-
+let customInput = document.querySelector("#custom1")
+console.log(customInput)
 
 
 
@@ -35,7 +36,7 @@ numberOfPeople.addEventListener("input",() => {
         numberOfPeople.style.border = "hsl(186, 14%, 43%) solid 2px";
         zeroNumOfPeople.style.display = "none";
      }})
-
+ 
 // On click functio to calculate tips 
 function myFunction(percentage) {
     var x = parseInt(billAmount.value) ; 
@@ -48,6 +49,17 @@ function myFunction(percentage) {
         totaAmountPerPerson.innerHTML = "0.00";
         totalTipPerPerson.innerHTML = "0.00";
     }
+  }
+
+  function myFunction1() {
+    const customNumber = document.querySelector("#custom1")
+    let customNum = customNumber.value
+    var x = parseInt(billAmount.value) ; 
+    let people = parseInt(numberOfPeople.value);
+    let tipAmount = x *customNum / 100;
+    let totalAmount = x + tipAmount
+    totalTipPerPerson.innerHTML = Math.round(x *customNum / 100 / people).toFixed(2);
+    totaAmountPerPerson.innerHTML =Math.round(totalAmount / people).toFixed(2);
   }
 
   function reset(){
@@ -65,6 +77,23 @@ function myFunction(percentage) {
   function customButton(){
     customBtn = customBtn.remove()
     let customInput = document.querySelector("#select-tips")
-    customInput.insertAdjacentHTML("beforeend", ` <input type="number" placeholder="0" class="custom-number" id="custom"></input>`); 
+    customInput.insertAdjacentHTML("beforeend", ` <input type="number" placeholder="0" class="custom-number" id="custom1" oninput="myFunction1()"></input>`); 
     
   }
+
+  
+
+// customInput.addEventListener("input",() => {  
+//     let customInput = customInput.value;
+//     console.log("sds")
+//     var x = parseInt(billAmount.value) ; 
+//     let people = parseInt(numberOfPeople.value);
+//     let tipAmount = x *customInput / 100;
+//     let totalAmount = x + tipAmount
+//     totalTipPerPerson.innerHTML = Math.round(x *customInput / 100 / people).toFixed(2);
+//     totaAmountPerPerson.innerHTML =Math.round(totalAmount / people).toFixed(2);
+//     if(isNaN(billAmount.value)|| billAmount.value == 0 || numberOfPeople.value == 0){
+//         totaAmountPerPerson.innerHTML = "0.00";
+//         totalTipPerPerson.innerHTML = "0.00";
+//     }})
+
