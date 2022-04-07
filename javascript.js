10e973a35b3ac11ab9da522b4430ca4d794a4451
Stalius4 <deivydasrimgaila@gmail.com>
2022-04-07 13:25:30 +0100

    mobile version without custom button
    only desktop version for css and custom button for js missing

--- javascript.js
+++ javascript.js
@@ -4,40 +4,50 @@ const totalTipPerPerson = document.querySelector("#total-tip-per-person");
 const numberOfPeople = document.querySelector("#numb-of-people");
 const zero = document.querySelector("#zero-top")
 const zeroMid = document.querySelector("#zero-mid")
-function myFunction(percentage) {
-    //1. take value from #bill amount and change it to number with parseInt()
-    let bill = parseInt(billAmount.value,) ; 
-    let people = parseInt(numberOfPeople.value);
-    let tipAmount = bill *percentage / 100;
-    let totalAmount = bill + tipAmount;
-   //2 if bill amount has no value  check with isNaN or value of 0
-    if(isNaN(bill) || bill == 0 && isNaN(people) || people ==0){
-    // Show message It cannot be zero and red border
+const inputs = document.querySelectorAll(".input")
+
+
+
+
+
+// Checking if bill amount is not NaN or 0 if yes, shows message with error
+billAmount.addEventListener("input",() => {  
+    if(isNaN(billAmount.value)|| billAmount.value == 0 ){
         billAmount.style.border = "orangered solid 2px";
         zero.style.display = "flex";
-        totalTipPerPerson.innerHTML = "0.00"
-        zeroMid.style.display = "flex";
+        totaAmountPerPerson.innerHTML = "0.00";
         totalTipPerPerson.innerHTML = "0.00";
-        numberOfPeople.style.border = "orangered solid 2px";
-    
-        
-    
-    }else if(!isNaN(bill) || bill > 0 )
-    {billAmount.style.border = "hsl(186, 14%, 43%) solid 2px";
+    }else{
+        billAmount.style.border = "hsl(186, 14%, 43%) solid 2px";
         zero.style.display = "none";
-        totalTipPerPerson.innerHTML = (bill *percentage / 100 / people).toFixed(2);
-        totaAmountPerPerson.innerHTML =(totalAmount / people).toFixed(2);
-    }else if(!isNaN(people) || people > 0)
-    {numberOfPeople.style.border = "hsl(186, 14%, 43%) solid 2px";
+    }})
+
+
+
+// Checking if number of people input is not NaN or 0 if yes, shows message with error    
+numberOfPeople.addEventListener("input",() => {  
+    if(isNaN(numberOfPeople.value)|| numberOfPeople.value == 0 ){
+         numberOfPeople.style.border = "orangered solid 2px";
+         zeroMid.style.display = "flex";
+        totaAmountPerPerson.innerHTML = "0.00";
+        totalTipPerPerson.innerHTML = "0.00";
+    }else{
+        numberOfPeople.style.border = "hsl(186, 14%, 43%) solid 2px";
         zeroMid.style.display = "none";
-        totalTipPerPerson.innerHTML = (bill *percentage / 100 / people).toFixed(2);
-        totaAmountPerPerson.innerHTML =(totalAmount / people).toFixed(2);
+     }})
+
+// On click functio to calculate tips 
+function myFunction(percentage) {
+    var x = parseInt(billAmount.value) ; 
+    let people = parseInt(numberOfPeople.value);
+    let tipAmount = x *percentage / 100;
+    let totalAmount = x + tipAmount
+    totalTipPerPerson.innerHTML = Math.round(x *percentage / 100 / people);
+    totaAmountPerPerson.innerHTML =Math.round(totalAmount / people);
+    if(isNaN(billAmount.value)|| billAmount.value == 0 ){
+        totaAmountPerPerson.innerHTML = "0.00";
+        totalTipPerPerson.innerHTML = "0.00";
     }
-
-
-    
-    
-    
   }
 
   function reset(){
@@ -45,11 +55,9 @@ function myFunction(percentage) {
   numberOfPeople.value = "";
   totaAmountPerPerson.innerHTML = "0.00";
   totalTipPerPerson.innerHTML = "0.00";
-
+  billAmount.style.border = "hsl(186, 14%, 43%) solid 2px";
+  zero.style.display = "none";
+  numberOfPeople.style.border = "hsl(186, 14%, 43%) solid 2px";
+  zeroMid.style.display = "none";
   }
-//   var input = document.getElementById('billAmount');
 
-//   if(input.value.length == 0)
-//       input.value = "Empty";
-
-//       console.log(input)
